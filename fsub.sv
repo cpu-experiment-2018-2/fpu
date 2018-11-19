2a5f72882b0641332cae9fcebed21c76b6fbc129
TDBTWD <39935669+TDBTWD@users.noreply.github.com>
2018-11-19 22:10:41 +0900

    fsubのレイテンシ2になった版(300MHz対応)

--- fsub.sv
+++ fsub.sv
@@ -20,17 +20,8 @@ assign hiseiki   = hiseiki_a || hiseiki_b;
 wire tashi1hiki0;
 assign tashi1hiki0 = adata[31] ^ bdata[31];
 
-wire sisuudeka_a;
-assign sisuudeka_a = adata[30:23] > bdata[30:23];
-
-wire sisuuonaji;
-assign sisuuonaji = adata[30:23] == bdata[30:23];
-
-wire kasuudeka_a;
-assign kasuudeka_a = adata[22:0] > bdata[22:0];
-
 wire deka_a;
-assign deka_a = sisuudeka_a || (sisuuonaji && kasuudeka_a);
+assign deka_a = adata[30:0] > bdata[30:0];
 
 wire deka_s;
 assign deka_s = deka_a ? adata[31] : ~bdata[31];
@@ -50,33 +41,35 @@ assign bhikua = bdata[30:23] - adata[30:23];
 wire [23:0] chibi_m;
 assign chibi_m = deka_a ? ({1'b1,bdata[22:0]} >> ahikub) : ({1'b1,adata[22:0]} >> bhikua);
 
-reg hiseiki_reg;
-reg hiseiki_a_reg;
-reg tashi1hiki0_reg;
-reg deka_s_reg;
-reg [7:0]  deka_e_reg;
-reg [23:0] deka_m_reg;
-reg [23:0] chibi_m_reg;
-reg [31:0] adata_reg_1;
-reg [31:0] bdata_reg_1;
-reg flag1;
-reg [4:0] address1;
+/*
 
-always@(posedge clk) begin
+wire hiseiki_reg;
+wire hiseiki_a_reg;
+wire tashi1hiki0_reg;
+wire deka_s_reg;
+wire [7:0]  deka_e_reg;
+wire [23:0] deka_m_reg;
+wire [23:0] chibi_m_reg;
+wire [31:0] adata_reg_1;
+wire [31:0] bdata_reg_1;
+wire flag1;
+wire [4:0] address1;
 
-hiseiki_reg <= hiseiki;
-hiseiki_a_reg <= hiseiki_a;
-tashi1hiki0_reg <= tashi1hiki0; 
-deka_s_reg <= deka_s;
-deka_e_reg <= deka_e;
-deka_m_reg <= deka_m;
-chibi_m_reg <= chibi_m;
-adata_reg_1 <= adata;
-bdata_reg_1 <= {~bdata[31],bdata[30:0]};
-flag1 <= flag_in;
-address1 <= address_in;
 
-end
+
+assign hiseiki_reg = hiseiki;
+assign hiseiki_a_reg = hiseiki_a;
+assign tashi1hiki0_reg = tashi1hiki0; 
+assign deka_s_reg = deka_s;
+assign deka_e_reg = deka_e;
+assign deka_m_reg = deka_m;
+assign chibi_m_reg = chibi_m;
+assign adata_reg_1 = adata;
+assign bdata_reg_1 = bdata;
+assign flag1 = flag_in;
+assign address1 = address_in;
+
+
 
 wire [12:0] Hcarrysub;
 assign Hcarrysub = tashi1hiki0_reg ? {deka_m_reg[23:13],1'b1} + {chibi_m_reg[23:13],1'b1} : {deka_m_reg[23:13],1'b0} - {chibi_m_reg[23:13],1'b1};
@@ -96,6 +89,8 @@ assign carry = tashi1hiki0_reg ? Lsub[13] : ~Lsub[13];
 wire [12:0] L;
 assign L = Lsub[12:0];
 
+*/
+
 reg hiseiki_reg_2;
 reg hiseiki_a_reg_2;
 reg s2_reg;
@@ -108,15 +103,15 @@ reg [4:0] address2;
 
 always@(posedge clk) begin
 
-hiseiki_reg_2 <= hiseiki_reg;
-hiseiki_a_reg_2 <= hiseiki_a_reg;
-s2_reg <= deka_s_reg;
-e2_reg <= deka_e_reg;
-kekka_reg <= carry ? {Hcarry,L} : {Hnocarry,L};
-adata_reg_2 <= adata_reg_1;
-bdata_reg_2 <= bdata_reg_1;
-flag2 <= flag1;
-address2 <= address1;
+hiseiki_reg_2 <= hiseiki;
+hiseiki_a_reg_2 <= hiseiki_a;
+s2_reg <= deka_s;
+e2_reg <= deka_e;
+kekka_reg <= tashi1hiki0 ? deka_m + chibi_m : deka_m - chibi_m;
+adata_reg_2 <= adata;
+bdata_reg_2 <= {~bdata[31],bdata[30:0]};
+flag2 <= flag_in;
+address2 <= address_in;
 
 end
 
@@ -125,58 +120,61 @@ flag_out <= flag2;
 address_out <= address2;
 if (hiseiki_reg_2) begin
 result <= hiseiki_a_reg_2 ? bdata_reg_2 : adata_reg_2;
-end else if (kekka_reg[24] == 1) begin
-result <= {s2_reg,e2_reg+8'd1,kekka_reg[23:1]};
-end else if (kekka_reg[23] == 1) begin
-result <= {s2_reg,e2_reg,kekka_reg[22:0]};
-end else if (kekka_reg[22] == 1) begin
-result <= (e2_reg < 1) ? 0 : {s2_reg,e2_reg-8'd1,kekka_reg[21:0],1'b0};
-end else if (kekka_reg[21] == 1) begin
-result <= (e2_reg < 2) ? 0 : {s2_reg,e2_reg-8'd2,kekka_reg[20:0],2'b0};
-end else if (kekka_reg[20] == 1) begin
-result <= (e2_reg < 3) ? 0 : {s2_reg,e2_reg-8'd3,kekka_reg[19:0],3'b0};
-end else if (kekka_reg[19] == 1) begin
-result <= (e2_reg < 4) ? 0 : {s2_reg,e2_reg-8'd4,kekka_reg[18:0],4'b0};
-end else if (kekka_reg[18] == 1) begin
-result <= (e2_reg < 5) ? 0 : {s2_reg,e2_reg-8'd5,kekka_reg[17:0],5'b0};
-end else if (kekka_reg[17] == 1) begin
-result <= (e2_reg < 6) ? 0 : {s2_reg,e2_reg-8'd6,kekka_reg[16:0],6'b0};
-end else if (kekka_reg[16] == 1) begin
-result <= (e2_reg < 7) ? 0 : {s2_reg,e2_reg-8'd7,kekka_reg[15:0],7'b0};
-end else if (kekka_reg[15] == 1) begin
-result <= (e2_reg < 8) ? 0 : {s2_reg,e2_reg-8'd8,kekka_reg[14:0],8'b0};
-end else if (kekka_reg[14] == 1) begin
-result <= (e2_reg < 9) ? 0 : {s2_reg,e2_reg-8'd9,kekka_reg[13:0],9'b0};
-end else if (kekka_reg[13] == 1) begin
-result <= (e2_reg < 10) ? 0 : {s2_reg,e2_reg-8'd10,kekka_reg[12:0],10'b0};
-end else if (kekka_reg[12] == 1) begin
-result <= (e2_reg < 11) ? 0 : {s2_reg,e2_reg-8'd11,kekka_reg[11:0],11'b0};
-end else if (kekka_reg[11] == 1) begin
-result <= (e2_reg < 12) ? 0 : {s2_reg,e2_reg-8'd12,kekka_reg[10:0],12'b0};
-end else if (kekka_reg[10] == 1) begin
-result <= (e2_reg < 13) ? 0 : {s2_reg,e2_reg-8'd13,kekka_reg[9:0],13'b0};
-end else if (kekka_reg[9] == 1) begin
-result <= (e2_reg < 14) ? 0 : {s2_reg,e2_reg-8'd14,kekka_reg[8:0],14'b0};
-end else if (kekka_reg[8] == 1) begin
-result <= (e2_reg < 15) ? 0 : {s2_reg,e2_reg-8'd15,kekka_reg[7:0],15'b0};
-end else if (kekka_reg[7] == 1) begin
-result <= (e2_reg < 16) ? 0 : {s2_reg,e2_reg-8'd16,kekka_reg[6:0],16'b0};
-end else if (kekka_reg[6] == 1) begin
-result <= (e2_reg < 17) ? 0 : {s2_reg,e2_reg-8'd17,kekka_reg[5:0],17'b0};
-end else if (kekka_reg[5] == 1) begin
-result <= (e2_reg < 18) ? 0 : {s2_reg,e2_reg-8'd18,kekka_reg[4:0],18'b0};
-end else if (kekka_reg[4] == 1) begin
-result <= (e2_reg < 19) ? 0 : {s2_reg,e2_reg-8'd19,kekka_reg[3:0],19'b0};
-end else if (kekka_reg[3] == 1) begin
-result <= (e2_reg < 20) ? 0 : {s2_reg,e2_reg-8'd20,kekka_reg[2:0],20'b0};
-end else if (kekka_reg[2] == 1) begin
-result <= (e2_reg < 21) ? 0 : {s2_reg,e2_reg-8'd21,kekka_reg[1:0],21'b0};
-end else if (kekka_reg[1] == 1) begin
-result <= (e2_reg < 22) ? 0 : {s2_reg,e2_reg-8'd22,kekka_reg[0:0],22'b0};
-end else if (kekka_reg[0] == 1) begin
-result <= (e2_reg < 23) ? 0 : {s2_reg,e2_reg-8'd23,23'b0};
 end else begin
+casex (kekka_reg) 
+25'b1xxxxxxxxxxxxxxxxxxxxxxxx: 
+result <= {s2_reg,e2_reg+8'd1,kekka_reg[23:1]};
+25'b01xxxxxxxxxxxxxxxxxxxxxxx: 
+result <= {s2_reg,e2_reg,kekka_reg[22:0]};
+25'b001xxxxxxxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 1) ? 0 : {s2_reg,e2_reg-8'd1,kekka_reg[21:0],1'b0};
+25'b0001xxxxxxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 2) ? 0 : {s2_reg,e2_reg-8'd2,kekka_reg[20:0],2'b0};
+25'b00001xxxxxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 3) ? 0 : {s2_reg,e2_reg-8'd3,kekka_reg[19:0],3'b0};
+25'b000001xxxxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 4) ? 0 : {s2_reg,e2_reg-8'd4,kekka_reg[18:0],4'b0};
+25'b0000001xxxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 5) ? 0 : {s2_reg,e2_reg-8'd5,kekka_reg[17:0],5'b0};
+25'b00000001xxxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 6) ? 0 : {s2_reg,e2_reg-8'd6,kekka_reg[16:0],6'b0};
+25'b000000001xxxxxxxxxxxxxxxx: 
+result <= (e2_reg < 7) ? 0 : {s2_reg,e2_reg-8'd7,kekka_reg[15:0],7'b0};
+25'b0000000001xxxxxxxxxxxxxxx: 
+result <= (e2_reg < 8) ? 0 : {s2_reg,e2_reg-8'd8,kekka_reg[14:0],8'b0};
+25'b00000000001xxxxxxxxxxxxxx: 
+result <= (e2_reg < 9) ? 0 : {s2_reg,e2_reg-8'd9,kekka_reg[13:0],9'b0};
+25'b000000000001xxxxxxxxxxxxx: 
+result <= (e2_reg < 10) ? 0 : {s2_reg,e2_reg-8'd10,kekka_reg[12:0],10'b0};
+25'b0000000000001xxxxxxxxxxxx: 
+result <= (e2_reg < 11) ? 0 : {s2_reg,e2_reg-8'd11,kekka_reg[11:0],11'b0};
+25'b00000000000001xxxxxxxxxxx: 
+result <= (e2_reg < 12) ? 0 : {s2_reg,e2_reg-8'd12,kekka_reg[10:0],12'b0};
+25'b000000000000001xxxxxxxxxx: 
+result <= (e2_reg < 13) ? 0 : {s2_reg,e2_reg-8'd13,kekka_reg[9:0],13'b0};
+25'b0000000000000001xxxxxxxxx: 
+result <= (e2_reg < 14) ? 0 : {s2_reg,e2_reg-8'd14,kekka_reg[8:0],14'b0};
+25'b00000000000000001xxxxxxxx: 
+result <= (e2_reg < 15) ? 0 : {s2_reg,e2_reg-8'd15,kekka_reg[7:0],15'b0};
+25'b000000000000000001xxxxxxx: 
+result <= (e2_reg < 16) ? 0 : {s2_reg,e2_reg-8'd16,kekka_reg[6:0],16'b0};
+25'b0000000000000000001xxxxxx: 
+result <= (e2_reg < 17) ? 0 : {s2_reg,e2_reg-8'd17,kekka_reg[5:0],17'b0};
+25'b00000000000000000001xxxxx: 
+result <= (e2_reg < 18) ? 0 : {s2_reg,e2_reg-8'd18,kekka_reg[4:0],18'b0};
+25'b000000000000000000001xxxx: 
+result <= (e2_reg < 19) ? 0 : {s2_reg,e2_reg-8'd19,kekka_reg[3:0],19'b0};
+25'b0000000000000000000001xxx: 
+result <= (e2_reg < 20) ? 0 : {s2_reg,e2_reg-8'd20,kekka_reg[2:0],20'b0};
+25'b00000000000000000000001xx: 
+result <= (e2_reg < 21) ? 0 : {s2_reg,e2_reg-8'd21,kekka_reg[1:0],21'b0};
+25'b000000000000000000000001x: 
+result <= (e2_reg < 22) ? 0 : {s2_reg,e2_reg-8'd22,kekka_reg[0:0],22'b0};
+25'b0000000000000000000000001: 
+result <= (e2_reg < 23) ? 0 : {s2_reg,e2_reg-8'd23,23'b0};
+25'b0:
 result <= 0;
+endcase
 end
 end
 endmodule
