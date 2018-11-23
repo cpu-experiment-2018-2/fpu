718083ea8a3a7310b2256cf2818e743a1f2024a8
TDBTWD <39935669+TDBTWD@users.noreply.github.com>
2018-11-23 15:43:58 +0900

    パイプライン化したfdivの、divとしてのテスト

--- test_fdiv.sv
+++ test_fdiv.sv
@@ -1,8 +1,8 @@
-`timescale 1ps / 1ps
+`timescale 1ns / 100ps
 `default_nettype none
 
 module test_fdiv
-  #(parameter NSTAGE = 11)
+  #(parameter NSTAGE = 4)
    ();
    logic [31:0] y;
    shortreal    fx1,fx2,fy;
@@ -18,11 +18,18 @@ module test_fdiv
    logic          en;
    logic 	clk;
    logic 	rstn;
-
+   logic        flagin;
+   logic [4:0]  addin;
+   logic        flagout;
+   logic [4:0]  addout;
 
    logic [31:0]	x1_reg[NSTAGE:0];
    logic [31:0]	x2_reg[NSTAGE:0];
    logic 	val[NSTAGE:0];
+   logic        flag[NSTAGE:0];
+   logic [4:0]  add[NSTAGE:0];
+   assign flagin = flag[0];
+   assign addin = add[0];
    
    logic [31:0] x1;
    logic [31:0] x2;
@@ -33,12 +40,15 @@ module test_fdiv
    logic [29:0] counter1;
    logic [29:0] counter2;
    logic [29:0] counter3;
+   
+   wire [31:0] fuga;
+   assign fuga = x1_reg[NSTAGE];
 
-   fdiv u1(en,x1,x2,y,done,busy,clk,rstn);
+   fdiv u1(x1,x2,y,clk,flagin,addin,flagout,addout);
 
    initial begin
-      // $dumpfile("test_fadd_p2.vcd");
-      // $dumpvars(0);
+     //  $dumpfile("test_fadd_p2.vcd");
+     //  $dumpvars(0);
       #1;
       rstn = 0;
       clk = 1;
@@ -50,7 +60,6 @@ module test_fdiv
       x2_reg[0] = 0;
       en <= 0;
       counter1 <= 0;
-    
   //                $display("counter1 %b ", counter1);
 
       #1;
@@ -111,99 +120,15 @@ module test_fdiv
                         x2_reg[0] <= {s2[0],j[7:0],m2};
 			val[0] <= 1;
 			en <= 1;
+			flag[0] <= $urandom();
+			add[0] <= $urandom();
 
                         #1;
 			clk = 0;
 			
 			
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-//			        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
+		
 			
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-			
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-			
-						
 			#1;
 			clk = 1;	
 			 counter1 <= counter1+1;
@@ -238,96 +163,7 @@ module test_fdiv
 		     clk = 0;
 		     
 		     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-
-		     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
-			     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		      counter1 <= counter1+1;
-	//	             $display("counter1 %b ", counter1);
-		     #1;
-		     clk = 0;
-		     
 	
-		     
-		     #1;
-		     clk = 1;
-		     val[0] <= 0;
-		     	 counter1 <= counter1+1;
-	//	     	        $display("counter1 %b ", counter1);		
-			#1;
-			clk = 0;
-			#1;
-			clk = 1;
-			val[0] <= 0;
-			 counter1 <= counter1+1;
-	//		        $display("counter1 %b ", counter1);
-			#1;
-			clk = 0;
-						
-
-			
 			#1;
 			clk = 1;
 			 counter1 <= counter1+1;
@@ -344,18 +180,21 @@ module test_fdiv
  //  $display ("x1 \n 4 %b \n 3 %b \n 2 %b \n 1 %b \n 0 %b \n", x1_reg[4], x1_reg[3],x1_reg[2],x1_reg[1],x1_reg[0]);
  //    $display ("x2 \n 4 %b \n 3 %b \n 2 %b \n 1 %b \n 0 %b \n", x2_reg[4], x2_reg[3],x2_reg[2],x2_reg[1],x2_reg[0]);
  //    $display("x1wire %b \n x2wire %b \n y %b", x1,x2,y);
- 
    if (counter1 == 2) begin
    counter2 <= 3;
       x1_reg[NSTAGE:1] <= x1_reg[NSTAGE-1:0];
       x2_reg[NSTAGE:1] <= x2_reg[NSTAGE-1:0];
       val[NSTAGE:1] <= val[NSTAGE-1:0];
+      flag[NSTAGE:1] <= flag[NSTAGE-1:0];
+      add[NSTAGE:1] <= add[NSTAGE-1:0];
   //           $display("counter2 %b ", counter2);
    end else begin
   counter2 <= counter2 + 1;
       x1_reg[NSTAGE:1] <= x1_reg[NSTAGE-1:0];
       x2_reg[NSTAGE:1] <= x2_reg[NSTAGE-1:0];
-      val[NSTAGE:1] <= val[NSTAGE-1:0];  
+      val[NSTAGE:1] <= val[NSTAGE-1:0];
+      flag[NSTAGE:1] <= flag[NSTAGE-1:0];
+      add[NSTAGE:1] <= add[NSTAGE-1:0];  
   //           $display("counter2 %b ", counter2);
    end 
    end
@@ -374,8 +213,17 @@ module test_fdiv
 	 fy = fx1 / fx2;
 	 fybit = $shortrealtobits(fy);
 
-
-	 if ((!(y[30:23]==0 && fybit[30:23]==0)) && y != fybit && y != fybit - 1 && y != fybit + 1 && fybit[30:23] != 8'd255 && y != fybit -2 && y != fybit + 2 && y != fybit - 3 && y != fybit + 3) begin
+/*	$display("kekka = %b",u1.kekka_reg);
+	$display ("deka = %b",u1.deka_m_reg);
+	$display("adata = %b",u1.adata);
+	$display("bdata = %b",u1.bdata);
+	$display("Hcarry = %b",u1.Hcarry);
+	$display("Hnocarry = %b",u1.Hnocarry);
+	$display("L = %b",u1.L);
+*/	if (flagout != flag[NSTAGE] || add[NSTAGE] != addout) begin
+$display("flag,add %b %b %b %b", flagout,flag[NSTAGE],addout,add[NSTAGE]);
+end 
+	 if ((!((fybit[30:23] == 0 || fuga[30:23] == 0) && y[30:23] == 0)) && (y < fybit - 7 || y > fybit + 7) && (!(fybit[30:23] == 255))) begin
             $display("x1, x2 = %b %b", x1_reg[NSTAGE], x2_reg[NSTAGE]);
             $display("%e %b ", fy, fybit);
             $display("%e %b \n", $bitstoshortreal(y), y);
